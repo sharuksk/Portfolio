d19ab69d730011d85b4652d30d9f315d48181d66
sharuksk <sharukajmal2@gmail.com>
2024-07-06 11:45:19 +0300

    resume updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
             </span></h2>
             <p style={{ color: "#666", letterSpacing: ".5px", marginTop: 2 }}> I am Competitive Programmer and I've been doing Algorithmic Problem solving since last 3 years. I am also interested in MERN Stack development and have made couple of projects in the same tech stack. I've a strong grasp on Data Structures & Algorithms and have a good command over Mathematics & Geometry.</p>
             <div className='btn-resume'>
-              <Button className='button23' href='https://drive.google.com/file/d/1jMiVsW8ewMLsChrxFpsfYKjjKPywDgZM/view' target='_blank'>Resume</Button>
+              <Button className='button23' href='https://drive.google.com/file/d/1s7Y0r5xp05vEvfTNJEe6NRRhPRU-XqkQ/view' target='_blank'>Resume</Button>
             </div>
           </div>
           <div className="right_div" alt="">
