c16fc50f4746368207e563ddb5559f69cfd4dcf7
sharuksk <sharukajmal2@gmail.com>
2026-01-05 20:00:17 +0300

    Talent Qatar project added

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -3,37 +3,46 @@ import todo from "../assets/todo.jpg";
 import traffic from "../assets/traffic.jpg";
 import imgtobas64 from "../assets/imgtobase64.png"
 import selfless from "../assets/selfless.jpeg"
+import talentQatarLogo from "../assets/talentQatarLogo.png"
+
 const ProjectData = [
     {
-        id:1,
+        id:2,
+        projectName:"Talent Qatar",
+        demo:"https://talent-qatar.onrender.com/",
+        imgsrc:talentQatarLogo,
+        desc:"Interview Hosting Platform"
+    },
+    {
+        id:2,
         projectName:"Personless Merchandise Store",
         demo:"https://main--merchandise-store.netlify.app/",
         imgsrc:selfless,
         desc:"PersonLess Merchandise Purchasing"
     },
     {
-        id:2,
+        id:3,
         projectName:"LinkedIN scraping",
         demo:"https://github.com/sharuksk/LinkedIN-Scrap",
         imgsrc:lkscrap,
         desc:"LinkedIN scraping tool (backend)"
     },
     {
-        id:3,
+        id:4,
         projectName:"To-Do List App",
         demo:"https://github.com/sharuksk/React-ToDoList",
         imgsrc:todo,
         desc:"Restaurent WebApp"
     },
     {
-        id:4,
+        id:5,
         projectName:"Traffic Analytics",
         demo:"https://traffic-analysis.netlify.app/",
         imgsrc:traffic,
         desc:"Traffic Analytics"
     },
     {
-        id:5,
+        id:6,
         projectName:"Image to Base64",
         demo:"https://main--best-image-base64-converter.netlify.app/",
         imgsrc:imgtobas64,
